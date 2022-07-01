e6201d3a5b92c7599bc6d85845103456e3d55635
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-07-01 19:26:29 +0600

    project completed

---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div class="text-center p-4 bg-dark">
+    <p className='text-warning'>© 2022 Copyright:</p>
+    <p class=" fw-bold text-success">ATG WORLD</p>
+  </div>
+        </footer>
+    );
+};
+
+export default Footer;
